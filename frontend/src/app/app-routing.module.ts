f676014f161e1b28836473976b42e402944633e1
Jawher Alkotaim <alkotaimja@Waseel.com>
2023-10-08 10:33:10 +0300

    add offer form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { ApplicantSuccessComponent } from './applicant-success/applicant-success
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { ApplicantDetailsComponent } from './applicant-details/applicant-details.component'
 import { AuthGuard } from './authentication/auth.guard';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { NotFoundComponent } from './not-found/not-found.component';
+import {OfferFormComponent} from './offer-form/offer-form.component';
 
 const routes: Routes = [
   // Placeholder route for role-based redirection after login
   { path: 'login-redirect', component: EmptyRedirectComponent, canActivate: [AuthGuard] },
 
   // Applicant routes
+  { path: 'applicant/offer/form', component: OfferFormComponent, canActivate: [AuthGuard], data: { roles: ['applicant-main'] }}, 
   { path: 'applicant/form', component: FormComponent, canActivate: [AuthGuard], data: { roles: ['applicant-main'] }}, 
   { path: 'applicant/submit', component: ApplicantSuccessComponent, canActivate: [AuthGuard], data: { roles: ['applicant-main'] }},
 
